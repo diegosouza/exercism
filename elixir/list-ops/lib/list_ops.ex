90dddd65139048de887cae841aeee8adaeeb0566
Diego Souza <diegosouza.br@gmail.com>
2021-03-19 18:50:11 -0300

    elixir/list-ops solved

--- elixir/list-ops/lib/list_ops.ex
+++ elixir/list-ops/lib/list_ops.ex
@@ -6,31 +6,54 @@ defmodule ListOps do
   # `++`, `--`, `hd`, `tl`, `in`, and `length`.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-  end
+  def count([_ | t]), do: count(t) + 1
+  def count([]), do: 0
 
-  @spec reverse(list) :: list
-  def reverse(l) do
-  end
+  @spec reverse(list, list) :: list
+  def reverse(list, reversed \\ [])
+  def reverse([h | t], reversed), do: reverse(t, [h | reversed])
+  def reverse([], reversed), do: reversed
 
-  @spec map(list, (any -> any)) :: list
-  def map(l, f) do
+  @spec map(list, (any -> any), list) :: list
+  def map(list, f, mapped \\ [])
+  def map([h | t], f, mapped) do
+    map(t, f, [f.(h) | mapped])
   end
+  def map([], _f, mapped), do: reverse(mapped)
 
-  @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
+  @spec filter(list, (any -> as_boolean(term)), list) :: list
+  def filter(list, f, filtered \\ [])
+  def filter([h | t], f, filtered) do
+    case f.(h) do
+      true -> filter(t, f, [h | filtered])
+      _ -> filter(t, f, filtered)
+    end
   end
+  def filter([], _f, filtered), do: reverse(filtered)
 
   @type acc :: any
   @spec reduce(list, acc, (any, acc -> acc)) :: acc
-  def reduce(l, acc, f) do
+  def reduce([], acc, _f), do: acc
+  def reduce([h | t], acc, f) do
+    new_acc = f.(h, acc)
+    reduce(t, new_acc, f)
   end
 
-  @spec append(list, list) :: list
-  def append(a, b) do
-  end
+  @spec append(list, list, list) :: list
+  def append(a, b, appended \\ [])
+  def append([], [h | t], appended), do: append([], t, [h | appended])
+  def append([h | t], [], appended), do: append(t, [], [h | appended])
+  def append([h | t], b, appended), do: append(t, b, [h | appended])
+  def append([], [], appended), do: reverse(appended)
 
-  @spec concat([[any]]) :: [any]
-  def concat(ll) do
+  @spec concat([[any]], [any]) :: [any]
+  def concat(ll, concatenated \\ [])
+  def concat([], concatenated), do: reverse(concatenated)
+  def concat([head | tail], concatenated) do
+    case head do
+      [h | t] -> concat([t | tail], [h | concatenated])
+      [x] -> concat(tail, [x | concatenated])
+      [] -> concat(tail, concatenated)
+    end
   end
 end
